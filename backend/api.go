6a0e4a4b7d0e0c5c7ae8a4e32a19f562fd61bdff
MrK <ahmadoubamba706@gmail.com>
2024-06-03 20:28:42 +0000

    Fourth commit

--- backend/api.go
+++ backend/api.go
@@ -67,5 +67,5 @@ func main() {
 	http.HandleFunc("/", loadPage)
 	http.HandleFunc("/getquestion", giveQuestion)
 	fmt.Println("Server listening on 192.168.1.20:8080")
-	http.ListenAndServe("0.0.0.0:8080", nil)
+	http.ListenAndServe(":8080", nil)
 }
